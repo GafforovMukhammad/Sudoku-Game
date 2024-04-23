33c18325df8e31467e47420308de243bc6075c01
Mukhammad <113128460+GafforovMukhammad@users.noreply.github.com>
2024-04-23 17:22:46 +0500

    Add files via upload

---
+++ gameSudoku.py
@@ -0,0 +1,81 @@
+def print_board(board):
+    """Prints the Sudoku board."""
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    """Finds an empty space in the board."""
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, column
+
+    return None
+
+def is_valid(board, num, pos):
+    """Checks whether the attempted move is valid."""
+
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    """Solves a sudoku board using backtracking."""
+    find = find_empty(board)
+    if not find:
+        return True  # Puzzle solved
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0  # Backtrack
+
+    return False
+
+board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+print("Sudoku Board:")
+print_board(board)
+solve(board)
+print("Solved Sudoku Board:")
+print_board(board)
